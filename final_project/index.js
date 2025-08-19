dd3a028fea099a3e613337321463044200923e3b
Marcos Mora <abreu760@hotmail.com>
2025-08-19 01:07:13 -0400

    update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,14 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session && req.session.authorization) {
+      // User is authenticated
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized access" });
+    }
+  });
 });
  
 const PORT =5000;
